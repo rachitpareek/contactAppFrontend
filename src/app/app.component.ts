85e97c3dc948e8fe67285188daef1b3d71c12715
Rachit Pareek <rachit@wppmbp201906272-wifi.ptc.ca.kp.org>
2019-07-15 17:19:10 -0700

    Add login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contactapp';
+
+  password;
+  correctPassword = 'password';
+  pwdCorrect = false;
+
+  /* The password logic also allows database query to be made before user enters site, so information is ready for them. */
+  addPassword() {
+    if (this.password == this.correctPassword) {
+      this.pwdCorrect = true;
+    } else {
+      alert("Incorrect password entered! Enter the word \"password\".");
+    }
+  }
+
 }
